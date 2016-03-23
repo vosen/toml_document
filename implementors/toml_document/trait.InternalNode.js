9160255d7c47c9202436217d4c63c70c1b02e8bf
Andrzej Janik <vosen@vosen.pl>
2016-03-23 21:14:35 +0100

    Docs for 0.1

---
+++ implementors/toml_document/trait.InternalNode.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];implementors['toml_document'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
